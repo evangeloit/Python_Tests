bfe735cf455f3a6b040b02aaa84c9292daf549fc
Evangelos Germenis <evangeloit@gmail.com>
2018-05-17 22:22:42 +0300

    Import modules

---
+++ my_module.py
@@ -0,0 +1,14 @@
+
+
+print('Imported my module...')
+
+test = 'Test string'
+
+def find_index(to_search, target):
+    '''Find the index of a value in a sequence '''
+
+    for i, value in enumerate(to_search):
+        if value == target:
+            return i
+
+    return -1
