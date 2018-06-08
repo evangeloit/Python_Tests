47499eb3efcb7cc8e1f588e2031d38d290bae3b0
Evangelos Germenis <evangeloit@gmail.com>
2018-06-09 00:25:33 +0300

    time_sleep(5)

--- Udacity/break_time.py
+++ Udacity/break_time.py
@@ -8,7 +8,7 @@ print("This program started on" + time.ctime())
 
 while (break_count < total_breaks):
     print(break_count)
-    time.sleep()
+    time.sleep(5)
     webbrowser.open('https://www.youtube.com/watch?v=1MGVSupOBVg')
     break_count= break_count + 1
 
